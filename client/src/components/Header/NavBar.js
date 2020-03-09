aba120632fcb2eb6d565e322f6ccdcd53f190b0b
DESKTOP-DQ05US4\amand <amanda.aribe@gmail.com>
2020-03-09 01:09:18 -0400

    Refined user interface and navigation

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -25,20 +25,37 @@ const AccountOptions = () => {
 }
 
 const NavBar = () => {
-    return (
-        <div className = "header">
+    return(
+<UserContext.Consumer>{context => {
+    if(context.isAuthenticated()) {
+        return (
+            <div className = "header">
+                <Link className = "nav-title" to="/">
+                    <div>Landing Page</div>
+                </Link>
+                <div className = "nav-items">
+                    <Link className = "nav-link" to='/Home'>Home</Link>
+                    <Link className = "nav-link" to='/Course'>Courses</Link>
+                    <Link className = "nav-link" to='/Store'>Store</Link>
+                    <Link className = "nav-link" to='/User'>Profile</Link>
+                    <AccountOptions/>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return(
+            <div className = "header">
             <Link className = "nav-title" to="/">
                 <div>Landing Page</div>
             </Link>
             <div className = "nav-items">
-                <Link className = "nav-link" to='/Home'>Home</Link>
-                <Link className = "nav-link" to='/Course'>Courses</Link>
-                <Link className = "nav-link" to='/Store'>Store</Link>
-                <Link className = "nav-link" to='/User'>Profile</Link>
                 <AccountOptions/>
             </div>
         </div>
-    )
+        )
+    }
+}}</UserContext.Consumer>);
 };
 
 export default NavBar;
