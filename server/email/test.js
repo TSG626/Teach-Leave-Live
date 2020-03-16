e452aad4443648a67f75582f92235f00bf30d54d
SimonBFrank <frank.simon@ufl.edu>
2020-03-16 14:55:59 -0400

    Welcome email now works

--- server/email/test.js
+++ server/email/test.js
@@ -1,24 +1,9 @@
-const createEmail = require('./createEmail');
-const fs = require('fs');
-const fetch = require('node-fetch');
-
-function saveEmail(email) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('Email.html', email, (err) => {
-            if (err) return reject(err);
-            return resolve();
-        });
-    });
-}
+var sendEmail = require('./sendEmail.js');
 
 const userInfo = {
+    email: "frank.simon20@gmail.com",
     firstName: "Simon",
     lastName: "Frank"
 }
 
-createEmail({ userInfo }).then((email) => {
-    // Here is where you would send the email to someone using your service of
-    // choiche.
-    // For this example we are just writing the result to disk.
-    return saveEmail(email);
-});
+sendEmail.welcomeEmail(userInfo);
