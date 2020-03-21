eed5daa4005448a1e728c502ef6086dc1b5510d7
120pjy <33128049+120pjy@users.noreply.github.com>
2020-03-20 21:20:45 -0400

    Create config.js
    config.js was deleted by gitignore. putting it back on

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+//Holds our secrets
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://admin:pass@bootcamp2-sajda.mongodb.net/test?retryWrites=true&w=majority',
+    },
+    secret: 'secret'
+};
