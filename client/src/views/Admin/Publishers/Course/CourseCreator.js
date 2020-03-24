e6ec2cfcb4e270a248bb0c169480a3cf9af88f7a
Spencer Bass <spencerbass1001@gmail.com>
2020-03-24 15:59:54 -0400

    Updated styling of course creator

--- client/src/views/Admin/Publishers/Course/CourseCreator.js
+++ client/src/views/Admin/Publishers/Course/CourseCreator.js
@@ -8,7 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import DeleteIcon from '@material-ui/icons/Delete'
 import {UserContext} from '../../../../contexts/UserContext';
-import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Grid } from '@material-ui/core';
 import API from '../../../../modules/API';
 import { Redirect } from 'react-router-dom';
 
@@ -58,34 +58,42 @@ function AuthorForm(props){
     }
 
     return(
-        <React.Fragment>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="author"
-                label="Authors"
-                name="author"
-                value={author}
-                onChange={(event) => setAuthor(event.target.value)}
-                onKeyPress={handleAuthorSubmit}
-            />
-            {props.authors.map((author, index) => {
-                return(
-                    <Button
-                        key={index}
-                        variant="contained"
-                        color="secondary"
-                        className={classes.button}
-                        endIcon={<DeleteIcon/>}
-                        onClick={(event) => removeAuthor(index)}
-                    >
-                        {author}
-                    </Button>
-                )
-            })}
-        </React.Fragment>   
+        <Grid container>
+            <Grid item xs={12}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="author"
+                    label="Authors"
+                    name="author"
+                    value={author}
+                    onChange={(event) => setAuthor(event.target.value)}
+                    onKeyPress={handleAuthorSubmit}
+                />
+            </Grid>
+            <Grid item container xs={12}>
+                {props.authors.map((author, index) => {
+                    return(
+                        <Grid item xs={12} sm={2}
+                            style={{paddingRight: 5, paddingBottom: 5}}
+                        >
+                            <Button
+                                key={index}
+                                variant="contained"
+                                color="secondary"
+                                className={classes.button}
+                                endIcon={<DeleteIcon/>}
+                                onClick={(event) => removeAuthor(index)}
+                            >
+                                {author}
+                            </Button>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Grid>   
     )
 }
 
@@ -102,36 +110,39 @@ function PriceForm(props){
     }
 
     return(
-        <FormControl>
-            <FormLabel>Price</FormLabel>
-            <RadioGroup onChange={handleRadioChange}>
-                <FormControlLabel 
-                    value='free' 
-                    label='Free' 
-                    control={<Radio/>}
-                />
-                <FormControlLabel 
-                    value='price' 
-                    label={
-                        (radio == 'price') && 
-                        <CurrencyTextField
-                                variant="outlined"
-                                currencySymbol="$"
-                                minimumValue={0}
-                                maximumValue={1000}
-                                margin="normal"
-                                fullWidth
-                                value={props.price}
-                                onChange={handlePriceChange}
-                                id="price"
-                                label="Price"
-                                name="price"
-                        /> || 'Payed'
-                    }
-                    control={<Radio/>}
-                />
-            </RadioGroup>
-        </FormControl>
+        <Grid container style={{padding: 15}}>
+
+            <FormControl>
+                <FormLabel>Price</FormLabel>
+                <RadioGroup onChange={handleRadioChange}>
+                    <FormControlLabel 
+                        value='free' 
+                        label='Free' 
+                        control={<Radio/>}
+                    />
+                    <FormControlLabel 
+                        value='price' 
+                        label={
+                            (radio == 'price') && 
+                            <CurrencyTextField
+                                    variant="outlined"
+                                    currencySymbol="$"
+                                    minimumValue={0}
+                                    maximumValue={1000}
+                                    margin="normal"
+                                    fullWidth
+                                    value={props.price}
+                                    onChange={handlePriceChange}
+                                    id="price"
+                                    label="Price"
+                                    name="price"
+                            /> || 'Payed'
+                        }
+                        control={<Radio/>}
+                    />
+                </RadioGroup>
+            </FormControl>
+        </Grid>
     )
 }
 
@@ -170,40 +181,51 @@ export default function CourseCreator() {
         <UserContext.Consumer>{(context) => {
             return(
                 <Container component="main" maxWidth="xl">
-                <CssBaseline/>
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                        Course Creator
-                    </Typography>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="title"
-                        label="Course Title"
-                        name="title"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        multiline
-                        id="description"
-                        label="Description"
-                        name="description"
-                    />
-                    <AuthorForm authors={authors} setAuthors={setAuthors}/>
-                    <PriceForm price={price} setPrice={setPrice}/>
-                    <Button
-                        color='primary'
-                        variant='outlined'
-                        onClick={handleSubmit}
-                    >
-                        Submit
-                    </Button>
-                </div>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography component="h1" variant="h5">
+                                Course Creator
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="title"
+                                label="Course Title"
+                                name="title"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                fullWidth
+                                multiline
+                                id="description"
+                                label="Description"
+                                name="description"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <AuthorForm authors={authors} setAuthors={setAuthors}/>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <PriceForm price={price} setPrice={setPrice}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                color='primary'
+                                variant='outlined'
+                                onClick={handleSubmit}
+                            >
+                                Submit
+                            </Button>
+                        </Grid>
+                    </Grid>
                 </Container>
             )}}
         </UserContext.Consumer>
