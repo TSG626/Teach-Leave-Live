e2e403443064d5385f7ab30e210fc9b5f2315708
Omer Amir <thegamectrlor@gmail.com>
2020-03-24 15:44:06 -0400

    Preparing to update BlogPublisher

--- client/src/views/Admin/Publishers/Blog/BlogPublisher.js
+++ client/src/views/Admin/Publishers/Blog/BlogPublisher.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import './BlogPublisher.css';
+import API from '../../../../modules/API'
+
+const UserContext = createContext();
+
+// const UserProvider = (props) => {
+//     const [authenticated, setAuthenticated] = useState(false);
+//     const [user, setUser] = useState({});
+
+//     const deauthenticateUser = () => {
+//         localStorage.removeItem('token');
+//         setAuthenticated(false);
+//     }
+
+//     const authenticateUser = (token) => {
+//         localStorage.setItem('token', token);
+//         setAuthenticated(true);
+//     }
+
+//     const isAuthenticated = () => {
+//         return authenticated;
+//     }
+
+//     useEffect(() => {
+//         if(user.username) return;
+//         if(localStorage.getItem('token') !== null){
+//             setAuthenticated(true);
+//         }
+//     }, [user]);
+
+//     useEffect(() => {
+//         let ignore = false;
+//         async function fetchData(){
+//             API.get('/api/user/').then(res => {
+//                 if(res.status == 200){
+//                     const {username, email, firstname, lastname, admin} = res.data;
+//                     setUser({
+//                         username: username,
+//                         email: email,
+//                         firstname: firstname,
+//                         lastname: lastname,
+//                         admin: admin,
+//                     });
+//                 }
+//             });
+//         }
+//         fetchData();
+//         return () => {ignore = true;}
+//     }, [authenticated]);
+
+//     const data = {
+//         //data
+//         user,
+//         setUser,
+//         authenticated,
+//         //functions
+//         authenticateUser,
+//         deauthenticateUser,
+//         isAuthenticated
+//     }; 
+
+//     return (
+//         <UserContext.Provider value={data}>
+//             {props.children}
+//         </UserContext.Provider>
+//     );
+// };
+
+// export {UserContext, UserProvider};
 
 export default function BlogPublisher() {
     return (
