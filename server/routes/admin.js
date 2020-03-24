737dfa33ff7f3147f2a95e205fab9f033f0ee9c1
Brian-Barker <59895517+Brian-Barker@users.noreply.github.com>
2020-03-24 15:33:21 -0400

    Revert "Merge branch 'master' into Forgot-Password"
    This reverts commit c45f9d965cb0b2369f53615cfaebc5d1818d7d77, reversing
    changes made to 5481480061d28ad75a57ce7b57a446a2371f1ce6.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,16 +1,10 @@
 const express = require('express'); 
-const passport = require('passport');
-const Course = require('../models/CourseModel');
 
 const router = express.Router();
 
-//Checks if admin
-router.use('/', (req, res, next) => {
-    if(req.user.admin === true){
-        next();
-    }else{
-        res.status(401).send({message: 'User is not an admin.'});
-    }
-});
+//TODO: Make a variable stored in the user profile denoting them as admin and making use here
+router.get('/', (req, res) => {
+    res.send('admin');
+})
 
 module.exports = router;
