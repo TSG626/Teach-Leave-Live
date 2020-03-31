5628f81f1cbd921ff4d4bd01ae89e199a3b0cee3
Omer Amir <Omer.Amir.Ocean@gmail.com>
2020-03-31 01:29:47 -0400

    Added back deleted files

---
+++ client/src/contexts/Admin/BlogContext.js
@@ -0,0 +1,63 @@
+import React, { createContext, useState, useEffect } from "react";
+import API from '../../modules/API'
+
+const BlogContext = createContext();
+
+const BlogProvider = (props) => {
+    const [blogList, setBlogList] = useState([]);
+    const [blog, setBlog] = useState({});
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        async function fetchData(){
+            API.get('/api/blog/').then(res => {
+                if(res.status == 200){
+                    setBlogList(res.data);
+                }
+            });
+        }
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if(id){
+            let ignore = false;
+            async function fetchData(){
+                API.get('/api/blog/', {id : id}).then(res => {
+                    if(res.status == 200){
+                        setBlog(res.data);
+                    }
+                });
+            }
+            fetchData();
+            return () => {ignore = true;}
+        }else{
+            return;
+        }
+    }, [id]);
+
+    function updateBlog(){
+        API.post(`/api/blog/${id}`, {id : id}).then(res => {
+            if(res.status == 200){
+                setBlog('');
+            }
+        });
+    };
+
+    const data = {
+        blogList,
+        setBlogList,
+        blog,
+        setBlog,
+        setId,
+        updateBlog,
+    };
+
+    return (
+        <BlogContext.Provider value={data}>
+            {props.children}
+        </BlogContext.Provider>
+    );
+};
+
+export {BlogContext, BlogProvider};
