793e5f18eba14373424a4c03f2186fb99fe11832
Amanda <amanda.aribe@gmail.com>
2020-03-31 00:45:01 -0400

    rearranged axios calls and fixed user editor a bit

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const Course = require('../models/CourseModel');
+const User = require('../models/UserModel')
 
 const router = express.Router();
 
@@ -12,5 +13,36 @@ router.use('/', (req, res, next) => {
         res.status(401).send({message: 'User is not an admin.'});
     }
 });
+
+//makes user admin
+router.post('/makeAdmin', async (req, res, next) => {
+    User.findOneAndUpdate(
+        { 'email': req.body.email },
+        { 'admin': req.body.admin },
+        {returnNewDocument: true}).then((user) => {
+            return res.status(200).json({
+                success: true,
+            });
+        }
+    );
+})
+
+//get list of all users
+router.get('/getAllUsers', async (req, res) => {
+    User.find({}, function(err, data) {
+        if(err)
+            return err;
+        return res.json(data);
+    })
+})
+
+//delete user
+router.post('/deleteUser', async (req, res, next) => {
+    User.remove({'username': req.body.username}).then((user) => {
+        return res.status(200).json({
+            success: true,
+        });
+    })
+})
 
 module.exports = router;
