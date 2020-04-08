05f942859bfbbf61c76845f56af51d9b3fe70bea
Omer Amir <Omer.Amir.Ocean@gmail.com>
2020-04-08 11:03:54 -0400

    Fixed blog poster to display parts of blog post; doesn't save after refresh

--- client/src/views/Admin/Publishers/Blog/BlogCreator.js
+++ client/src/views/Admin/Publishers/Blog/BlogCreator.js
@@ -32,38 +32,6 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-function BodyForm(props){
-    const classes = useStyles();
-    const [body, setBody] = useState('');
-
-    return(
-        <Grid container>
-            <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="body"
-                    label="Body"
-                    name="body"
-                    value={body}
-                    onChange={(event) => setBody(event.target.value)}
-                />
-            </Grid>
-            <Grid item container xs={12}>
-                {/* {props.body.map((body, index) => {
-                    return(
-                        <Grid item xs={12} sm={2}
-                            style={{paddingRight: 5, paddingBottom: 5}}>
-                        </Grid>
-                    )
-                })} */}
-            </Grid>
-        </Grid>
-    )
-}
-
 export default function BlogCreator(props) {
     const classes = useStyles();
     
@@ -76,28 +44,19 @@ export default function BlogCreator(props) {
     function handleSubmit(){
         API.post('/api/blog/', {
             title: document.getElementById('title').value,
-            authors: authors,
             body: document.getElementById('body').value,
+            authors: authors
         }).then(res => {
-            //console.log(res.data);
             if(res.status == 200){
-                props.setBody(res.data.body);
-                props.setAuthors(res.data.authors);
-
-                setTitle(res.data.title);
-                setBody(res.data.body);
-                setAuthors(res.data.authors);
-                setId(res.data.id);
+                console.log(res.data);
+                props.setBlogList(props.blogList.concat(res.data));
+                props.setAdding(false);
+                //window.location.reload(false); FIXME: Uncomment later
+                return false;
             }
         }).catch(err => {
             setErrors(err);
         })
-    }
-
-    if(id != ''){
-        return(
-            <Redirect to={`/Admin/Blog/Edit/${id}/`}/>
-        )
     }
     
     return (
@@ -118,6 +77,7 @@ export default function BlogCreator(props) {
                         id="title"
                         label="Title"
                         name="title"
+                        onChange={(event) => setTitle(event.target.value)}
                     />
                 </Grid>
                 <Grid item xs={12}>
