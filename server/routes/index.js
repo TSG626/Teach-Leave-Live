88d5a64eee0e04a273035230fef6e0101bd7701a
Amanda <amanda.aribe@gmail.com>
2020-04-11 17:31:13 -0400

    added basic checkout functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.use('/', require('./auth.js'));
@@ -8,5 +7,7 @@ router.use('/admin', require('./admin.js'));
 router.use('/blog', require('./blog.js'));
 router.use('/course', require('./course.js'));
 router.use('/user', require('./user.js'));
+router.use('/cart', require('./cart.js'));
+
 
 module.exports = router;
