6b3e5171fd0f537fca707f08b0d51f9ef45719df
Amanda <amanda.aribe@gmail.com>
2020-04-17 19:06:14 -0400

    store + shopping cart basic functionality

--- client/src/views/Store/Cart/Cart.js
+++ client/src/views/Store/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Redirect, useHistory, withRouter, BrowserRouter, Switch } from 'react-router-dom';
 import { CssBaseline, Paper, TableCell, TableContainer, TableRow, TableHead, Box, Table, makeStyles} from '@material-ui/core';
 import { Typography } from '@material-ui/core';
@@ -6,6 +6,8 @@ import Button from '@material-ui/core/Button';
 import StripeCheckout from "react-stripe-checkout"
 import API from '../../../modules/API'
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import { UserContext } from '../../../contexts/UserContext';
+
 const useStyles = makeStyles({
     table: {
       margin: 'auto'
@@ -14,19 +16,35 @@ const useStyles = makeStyles({
 
 const Cart = () => {
     //array of products stored
-    const [products, setProduct] = useState([{
-        name: "React from FB",
-        price: 10,
-        productBy: "facebook"
-    }]);
+    const [products, setProducts] = useState([]);
+    const userInfo = useContext(UserContext);
+    const [changed, setChange] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const [names, setNames] = useState("");
+
+    useEffect(() => {
+        API.get('/api/user/getCart', {'username': userInfo.user.username}).then(res=>{
+            for (var i = 0; i < res.data.length; i++) {
+                API.get('/api/course/getCourse', {'id': res.data[i]}).then(res2=>{
+                    setProducts([...products, res2.data]);
+                })
+            }
+        }).catch(err=>{
+            console.log(err);
+        });
+    }, [counter]);
+    if(counter === 0) {
+        setCounter(1);
+        setChange(true);
+    }
     var count = 0;
     var subcount = 0;
-   // var taxes = 0;
     var service = .3;
     let history = useHistory();
 
     const makePayment = token => {
-        API.post('/api/cart/payment', {product: products[0], token: token}).then(res=>{
+
+        API.post('/api/cart/payment', {product: {name: userInfo.user.username + "'s items", price: count.toFixed(2) }, token: token}).then(res=>{
             console.log("RESPONSE", res);
             const {status} = res;
             console.log("STATUS", status);
@@ -63,13 +81,14 @@ const Cart = () => {
                             <TableCell></TableCell>
                         </TableRow>
                         {products.map(item => {
+                            console.log(item)
                             subcount += item.price;
                             //taxes += subcount * .07;
                             service += subcount * .029;
                             count = subcount + service;
                             return(
                                 <TableRow>
-                                    <TableCell>{item.name}</TableCell>
+                                    <TableCell>{item.title}</TableCell>
                                     <TableCell align="right">{"$" + item.price.toFixed(2)}</TableCell>
                                     <TableCell align="right"><DeleteOutlineIcon/></TableCell>
                                 </TableRow>
@@ -100,7 +119,7 @@ const Cart = () => {
                         </Table>
                     </TableContainer>
                 </Box>
-                    <StripeCheckout stripeKey={process.env.REACT_APP_KEY} token={makePayment} name="Teach. Leave. Live." amount={count * 100}>
+                    <StripeCheckout stripeKey={process.env.REACT_APP_KEY} token={makePayment} name="Teach. Leave. Live." amount={count.toFixed(2) * 100}>
                         <Button variant="contained" color="primary">Checkout</Button>
                     </StripeCheckout>
             </div>
