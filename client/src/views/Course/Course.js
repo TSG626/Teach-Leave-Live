c4fd2d6e221ae8e248e7ae3ccdb65c2ce780ce31
Amanda <amanda.aribe@gmail.com>
2020-04-19 12:29:32 -0400

    fix

--- client/src/views/Course/Course.js
+++ client/src/views/Course/Course.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Container, Grid, Box, CardContent, Card, Button } from "@material-ui/core";
 import API from "../../modules/API";
-import { Switch, Route, Redirect, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import CourseViewer from "./CourseViewer/CourseViewer";
 import { makeStyles } from '@material-ui/core/styles';
+import Store from "../Store/Store";
 
 const useStyles = makeStyles({
   root: {
@@ -29,6 +30,13 @@ function Landing(courses) {
       <Box m={3}>
         <Typography variant="h2" align="center">Courses</Typography>
     </Box>
+    {courses.length === 0 ? <React.Fragment>
+      <Typography align="center">No courses? Lets buy some!</Typography>
+        <Box mt={2} align="center">
+          <Button variant="contained" component={Link} to="/Store/">Course Store</Button>
+        </Box>
+      </React.Fragment>:
+    <React.Fragment>
       <Grid container justify="center" spacing={3}>
         {courses.map(course => {
           return(
@@ -58,6 +66,8 @@ function Landing(courses) {
         })}
       </Grid>
     </React.Fragment>
+    }
+    </React.Fragment>
   );
 }
 
@@ -84,6 +94,7 @@ export default function Course({ match }) {
       <Switch>
         <Route exact path={`${match.path}/`} component={() => Landing(courses)} />
         <Route path={`${match.path}/:id/`} component={CourseViewer} />
+        <Route exact path="Store/" component={Store} />
       </Switch>
     </Container>
   );
