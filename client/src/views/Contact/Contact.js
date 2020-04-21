72c43fc74bd669bc5d6e106720b067bef133a4d7
Brian-Barker <briantbarker@ufl.edu>
2020-04-20 20:27:53 -0400

    Cosmetic changes

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import API from "../../modules/API";
 import { Input } from "@material-ui/core";
@@ -43,51 +44,55 @@ const Contact = () => {
   const classes = useStyles();
   return !sent ? (
     <div>
-      <div class="title">
-        <h1>Teach. Leave. Live.</h1>
-      </div>
-      <div class="message">
-        <Typography component="h1" variant="h5">
-          Email us
-        </Typography>
-        <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
-          <Input
-            className={classes.input}
-            placeholder="message"
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            multiline
-            id="message"
-            label="Message"
-            name="message"
-            autoComplete="message"
-            autoFocus
-          />
-          <Input
-            className={classes.input}
-            placeholder="email"
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Send
-          </Button>
-        </form>
-      </div>
+      <Box p={4}>
+        <div class="title">
+          <h1>Teach. Leave. Live.</h1>
+        </div>
+      </Box>
+      <Box p={4}>
+        <div class="message">
+          <Typography component="h1" variant="h5">
+            Email us
+          </Typography>
+          <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
+            <Input
+              className={classes.input}
+              placeholder="message"
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              multiline
+              id="message"
+              label="Message"
+              name="message"
+              autoComplete="message"
+              autoFocus
+            />
+            <Input
+              className={classes.input}
+              placeholder="email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Send
+            </Button>
+          </form>
+        </div>
+      </Box>
     </div>
   ) : (
     <div>
