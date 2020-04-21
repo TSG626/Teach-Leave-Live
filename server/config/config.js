6f027ed2c8109736cebdef16138cc7667892121b
Amanda <amanda.aribe@gmail.com>
2020-04-20 22:41:50 -0400

    tll stripe api keys set

--- server/config/config.js
+++ server/config/config.js
@@ -4,8 +4,8 @@ module.exports = {
         uri: 'mongodb+srv://admin:pass@bootcamp2-sajda.mongodb.net/test?retryWrites=true&w=majority',
     },
     stripe: {
-        sk: "sk_test_ieIIqneiyAZukzPjJQ206yPH00lm7p2eTX",
-        pk: "pk_test_2qY3y4KNhG7xsgr5BTiqrO4S00oaC039hd" 
+        sk: "sk_test_rSnkHsXYWWZ1nQKYOFhwcaIh00SRuEnF6W",
+        pk: "pk_test_agP7UM32ocVL4l4ok94sTOud00XmXkuIiJ" 
     },
     secret: 'secret'
 };
